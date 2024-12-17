b53909e330f9343a51f845529f9a65c361fc05b2
chandan0507 <chandanubale8@gmail>
2024-12-17 21:04:22 +0530

    Included few more variables

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from userInput import takeUserInput, takeExpectedResult
 from writerfunc import writeUserInput
 
-captureTestCaseName = takeUserInput('Please provide the name of the test case & Hit \'q\' to quit: ')
-if captureTestCaseName == 'q':
-    quit()
-captureUrl = takeUserInput('Please provide the url to  : ')
-captureVariableName = takeUserInput('Please provide variable name to store the api body : ')
-captureApiBody = takeUserInput('Please provide the full api body : ')
-captureExpectedResult = takeExpectedResult('Please provide the expected results & Hit \'r\' to return back to main program : ')
-writeUserInput(captureTestCaseName, captureUrl, captureVariableName, captureApiBody, captureExpectedResult)
+captureTestCaseName = takeUserInput('Please provide the name of the test case : ')
+fileName = captureTestCaseName+".py"
+f = open(fileName, 'a')
+f.write(f'from apiCall import apiCallFrom\n\n\n')
+f.close()
+
+count=0
+
+while True:
+    incremental = str(count)
+    captureUrl = takeUserInput('Please provide the url to api OR hit \'q\' to quit : ')
+    if captureUrl == 'q':
+        quit()
+    captureVariableName = takeUserInput('Please provide variable name to store the api body : ')
+    captureApiBody = takeUserInput('Please provide the full api body : ')
+    captureDescription = takeUserInput('Please provide a description of case performed : ')
+    captureExpectedResult = takeExpectedResult('Please provide the expected results & Hit \'r\' to return back to main program : ')
+    writeUserInput(fileName, captureUrl, captureVariableName, captureApiBody, captureDescription, captureExpectedResult, incremental)
+    count+=1
